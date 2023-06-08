d0cea219d5de213d7cfeb534709545f345e4ca12
nonchalamment <nonchalamment@gmail.com>
2023-06-08 09:37:33 -0400

    implement delete functionality

--- main_app/views.py
+++ main_app/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Clothing
 
-closet = [
-  Clothing('Energy Bra', 'Sports Bra', 'Luxtreme', 68),
-  Clothing('Align Pant HR 25in', 'Leggings', "Nulu", 98),
-  Clothing('Invigorate HR Short 10in', 'Shorts', "Everlux", 68),
-  Clothing('Court Rival HR Skirt', 'Skirt', "Swift", 68)
-]
-
-# Create your views here.
 def home(request):
   return render(request, 'home.html')
 
