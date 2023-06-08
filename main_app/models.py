4327dfef2cf4225eb296f1fe2473bfd04734c371
nonchalamment <nonchalamment@gmail.com>
2023-06-08 10:49:20 -0400

    implement auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class Clothing(models.Model):
   category = models.CharField(max_length=50)
   material = models.TextField(max_length=100)
   msrp = models.IntegerField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,56 @@
-from django.shortcuts import render
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
 from .models import Clothing
 
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def clothing_index(request):
-  closet = Clothing.objects.all()
+  closet = Clothing.objects.filter(user=request.user)
   return render(request, 'closet/index.html', { 'closet': closet })
 
+@login_required
 def clothing_detail(request, clothing_id):
   clothing = Clothing.objects.get(id=clothing_id)
   return render(request, 'closet/detail.html', { 'clothing': clothing })
 
-class ClothingCreate(CreateView):
+class ClothingCreate(LoginRequiredMixin, CreateView):
   model = Clothing
-  fields = '__all__'
+  fields = ['name', 'category', 'msrp']
 
-class ClothingUpdate(UpdateView):
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class ClothingUpdate(LoginRequiredMixin, UpdateView):
   model = Clothing
   fields = ['category', 'material', 'msrp']
 
-class ClothingDelete(DeleteView):
+class ClothingDelete(LoginRequiredMixin, DeleteView):
   model = Clothing
   success_url = '/closet/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('clothing-index')
+    else:
+      error_message = 'Invalid signup - try again!'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
